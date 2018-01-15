1e25460616052f3236bc5996484aa1206b2521b5
Arewa Olakunle <danceasarxx@gmail.com>
2018-01-15 02:40:48 +0100

    Errors where being printed to standard output
    Switched to eprintln! for error reporting.

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,12 @@ use minigrep::io::Config;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let conf = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
     if let Err(e) = minigrep::run(conf) {
-        println!("Application Error: {}", e);
+        eprintln!("Application Error: {}", e);
         process::exit(1);
     };
 }
